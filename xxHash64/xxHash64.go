875de766ff3cdf28411845c6c43da8de97223a1c
glestum <glestum@salesforce.com>
2017-07-09 12:21:50 -0700

    use inlined functions for rol

--- xxHash64/xxHash64.go
+++ xxHash64/xxHash64.go
@@ -82,14 +82,10 @@ func (xxh *xxHash) Write(input []byte) (int, error) {
 		xxh.bufused += len(input) - r
 
 		// fast rotl(31)
-		p64 := xxh.v1 + u64(xxh.buf[:])*prime64_2
-		xxh.v1 = (p64<<31 | p64>>33) * prime64_1
-		p64 = xxh.v2 + u64(xxh.buf[8:])*prime64_2
-		xxh.v2 = (p64<<31 | p64>>33) * prime64_1
-		p64 = xxh.v3 + u64(xxh.buf[16:])*prime64_2
-		xxh.v3 = (p64<<31 | p64>>33) * prime64_1
-		p64 = xxh.v4 + u64(xxh.buf[24:])*prime64_2
-		xxh.v4 = (p64<<31 | p64>>33) * prime64_1
+		xxh.v1 = rol31(xxh.v1+u64(xxh.buf[:])*prime64_2) * prime64_1
+		xxh.v2 = rol31(xxh.v2+u64(xxh.buf[8:])*prime64_2) * prime64_1
+		xxh.v3 = rol31(xxh.v3+u64(xxh.buf[16:])*prime64_2) * prime64_1
+		xxh.v4 = rol31(xxh.v4+u64(xxh.buf[24:])*prime64_2) * prime64_1
 		p = r
 		xxh.bufused = 0
 	}
@@ -100,14 +96,10 @@ func (xxh *xxHash) Write(input []byte) (int, error) {
 		ptr := uintptr(unsafe.Pointer(&input[p]))
 		for n := n - 32; p <= n; p += 32 {
 			sub := (*[32]byte)(unsafe.Pointer(ptr))
-			p64 := xxh.v1 + u64(sub[:])*prime64_2
-			xxh.v1 = (p64<<31 | p64>>33) * prime64_1
-			p64 = xxh.v2 + u64(sub[8:])*prime64_2
-			xxh.v2 = (p64<<31 | p64>>33) * prime64_1
-			p64 = xxh.v3 + u64(sub[16:])*prime64_2
-			xxh.v3 = (p64<<31 | p64>>33) * prime64_1
-			p64 = xxh.v4 + u64(sub[24:])*prime64_2
-			xxh.v4 = (p64<<31 | p64>>33) * prime64_1
+			xxh.v1 = rol31(xxh.v1+u64(sub[:])*prime64_2) * prime64_1
+			xxh.v2 = rol31(xxh.v2+u64(sub[8:])*prime64_2) * prime64_1
+			xxh.v3 = rol31(xxh.v3+u64(sub[16:])*prime64_2) * prime64_1
+			xxh.v4 = rol31(xxh.v4+u64(sub[24:])*prime64_2) * prime64_1
 			ptr += 32
 		}
 	}
@@ -122,45 +114,37 @@ func (xxh *xxHash) Write(input []byte) (int, error) {
 func (xxh *xxHash) Sum64() uint64 {
 	var h64 uint64
 	if xxh.totalLen >= 32 {
-		h64 = ((xxh.v1 << 1) | (xxh.v1 >> 63)) +
-			((xxh.v2 << 7) | (xxh.v2 >> 57)) +
-			((xxh.v3 << 12) | (xxh.v3 >> 52)) +
-			((xxh.v4 << 18) | (xxh.v4 >> 46))
+		h64 = rol1(xxh.v1) + rol7(xxh.v2) + rol12(xxh.v3) + rol18(xxh.v4)
 
 		xxh.v1 *= prime64_2
-		h64 ^= ((xxh.v1 << 31) | (xxh.v1 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4
-
 		xxh.v2 *= prime64_2
-		h64 ^= ((xxh.v2 << 31) | (xxh.v2 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4
-
 		xxh.v3 *= prime64_2
-		h64 ^= ((xxh.v3 << 31) | (xxh.v3 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4
-
 		xxh.v4 *= prime64_2
-		h64 ^= ((xxh.v4 << 31) | (xxh.v4 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4 + xxh.totalLen
+
+		h64 = (h64^(rol31(xxh.v1)*prime64_1))*prime64_1 + prime64_4
+		h64 = (h64^(rol31(xxh.v2)*prime64_1))*prime64_1 + prime64_4
+		h64 = (h64^(rol31(xxh.v3)*prime64_1))*prime64_1 + prime64_4
+		h64 = (h64^(rol31(xxh.v4)*prime64_1))*prime64_1 + prime64_4
+
+		h64 += xxh.totalLen
 	} else {
 		h64 = xxh.seed + prime64_5 + xxh.totalLen
 	}
 	p := 0
 	n := xxh.bufused
 	for n := n - 8; p <= n; p += 8 {
-		p64 := u64(xxh.buf[p:p+8]) * prime64_2
-		h64 ^= ((p64 << 31) | (p64 >> 33)) * prime64_1
-		h64 = ((h64<<27)|(h64>>37))*prime64_1 + prime64_4
+		h64 ^= rol31(u64(xxh.buf[p:p+8])*prime64_2) * prime64_1
+		h64 = rol27(h64)*prime64_1 + prime64_4
 	}
 	if p+4 <= n {
 		sub := xxh.buf[p : p+4]
 		h64 ^= (uint64(sub[0]) | uint64(sub[1])<<8 | uint64(sub[2])<<16 | uint64(sub[3])<<24) * prime64_1
-		h64 = ((h64<<23)|(h64>>41))*prime64_2 + prime64_3
+		h64 = rol23(h64)*prime64_2 + prime64_3
 		p += 4
 	}
 	for ; p < n; p++ {
 		h64 ^= uint64(xxh.buf[p]) * prime64_5
-		h64 = ((h64 << 11) | (h64 >> 53)) * prime64_1
+		h64 = rol11(h64) * prime64_1
 	}
 
 	h64 ^= h64 >> 33
@@ -189,38 +173,27 @@ func Checksum(input []byte, seed uint64) uint64 {
 			ptr := uintptr(unsafe.Pointer(&input[p]))
 			for n := n - 32; p <= n; p += 32 {
 				sub := (*[32]byte)(unsafe.Pointer(ptr))
-				p64 := v1 + u64(sub[:])*prime64_2
-				v1 = (p64<<31 | p64>>33) * prime64_1
-				p64 = v2 + u64(sub[8:])*prime64_2
-				v2 = (p64<<31 | p64>>33) * prime64_1
-				p64 = v3 + u64(sub[16:])*prime64_2
-				v3 = (p64<<31 | p64>>33) * prime64_1
-				p64 = v4 + u64(sub[24:])*prime64_2
-				v4 = (p64<<31 | p64>>33) * prime64_1
+				v1 = rol31(v1+u64(sub[:])*prime64_2) * prime64_1
+				v2 = rol31(v2+u64(sub[8:])*prime64_2) * prime64_1
+				v3 = rol31(v3+u64(sub[16:])*prime64_2) * prime64_1
+				v4 = rol31(v4+u64(sub[24:])*prime64_2) * prime64_1
 				ptr += 32
 			}
 		}
 
-		h64 = ((v1 << 1) | (v1 >> 63)) +
-			((v2 << 7) | (v2 >> 57)) +
-			((v3 << 12) | (v3 >> 52)) +
-			((v4 << 18) | (v4 >> 46))
+		h64 = rol1(v1) + rol7(v2) + rol12(v3) + rol18(v4)
 
 		v1 *= prime64_2
-		h64 ^= ((v1 << 31) | (v1 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4
-
 		v2 *= prime64_2
-		h64 ^= ((v2 << 31) | (v2 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4
-
 		v3 *= prime64_2
-		h64 ^= ((v3 << 31) | (v3 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4
-
 		v4 *= prime64_2
-		h64 ^= ((v4 << 31) | (v4 >> 33)) * prime64_1
-		h64 = h64*prime64_1 + prime64_4 + uint64(n)
+
+		h64 = (h64^(rol31(v1)*prime64_1))*prime64_1 + prime64_4
+		h64 = (h64^(rol31(v2)*prime64_1))*prime64_1 + prime64_4
+		h64 = (h64^(rol31(v3)*prime64_1))*prime64_1 + prime64_4
+		h64 = (h64^(rol31(v4)*prime64_1))*prime64_1 + prime64_4
+
+		h64 += uint64(n)
 
 		input = input[p:]
 		n -= p
@@ -234,19 +207,18 @@ func Checksum(input []byte, seed uint64) uint64 {
 		p := 0
 		for n := n - 8; p <= n; p += 8 {
 			sub := input[p : p+8]
-			p64 := u64(sub) * prime64_2
-			h64 ^= ((p64 << 31) | (p64 >> 33)) * prime64_1
-			h64 = ((h64<<27)|(h64>>37))*prime64_1 + prime64_4
+			h64 ^= rol31(u64(sub)*prime64_2) * prime64_1
+			h64 = rol27(h64)*prime64_1 + prime64_4
 		}
 		if p+4 <= n {
 			sub := input[p : p+4]
 			h64 ^= (uint64(sub[3])<<24 | uint64(sub[2])<<16 | uint64(sub[1])<<8 | uint64(sub[0])) * prime64_1
-			h64 = ((h64<<23)|(h64>>41))*prime64_2 + prime64_3
+			h64 = rol23(h64)*prime64_2 + prime64_3
 			p += 4
 		}
 		for ; p < n; p++ {
 			h64 ^= uint64(input[p]) * prime64_5
-			h64 = ((h64 << 11) | (h64 >> 53)) * prime64_1
+			h64 = rol11(h64) * prime64_1
 		}
 	}
 
@@ -262,4 +234,35 @@ func Checksum(input []byte, seed uint64) uint64 {
 func u64(buf []byte) uint64 {
 	// go compiler recognizes this pattern and optimizes it on little endian platforms
 	return uint64(buf[0]) | uint64(buf[1])<<8 | uint64(buf[2])<<16 | uint64(buf[3])<<24 | uint64(buf[4])<<32 | uint64(buf[5])<<40 | uint64(buf[6])<<48 | uint64(buf[7])<<56
+}
+
+func rol1(u uint64) uint64 {
+	return u<<1 | u>>63
+}
+
+func rol7(u uint64) uint64 {
+	return u<<7 | u>>57
+}
+
+func rol11(u uint64) uint64 {
+	return u<<11 | u>>53
+}
+
+func rol12(u uint64) uint64 {
+	return u<<12 | u>>52
+}
+
+func rol18(u uint64) uint64 {
+	return u<<18 | u>>46
+}
+
+func rol23(u uint64) uint64 {
+	return u<<23 | u>>41
+}
+
+func rol27(u uint64) uint64 {
+	return u<<27 | u>>37
+}
+func rol31(u uint64) uint64 {
+	return u<<31 | u>>33
 }
